96cec423e5efe9523362a5cd04b3f44eabbcd3b4
Anuj-kiet <53224310+Anuj-kiet@users.noreply.github.com>
2019-10-30 11:36:12 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch[100],s[100];
+    printf("C\n");
+scanf("%c",&ch);
+
+printf("Language\n");
+scanf("%s",&ch);
+printf("Welcome To C!!\n");
+scanf("%d",&ch);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
